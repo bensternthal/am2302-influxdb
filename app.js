12f8852bf8e4d0cbb4d8e6f480d280dc902e7b16
Benjamin Sternthal <ben@devpatch.com>
2018-02-03 12:09:05 -0800

    Beautifyed.

--- app.js
+++ app.js
@@ -10,23 +10,25 @@ const Delay = process.env.UPDATE_FREQUENCY;
 
 // Create & Configure Slackbot
 let bot = new SlackBot({
-    token: process.env.SLACK_API_TOKEN,
-    name: process.env.SLACK_BOT_NAME,
+	token: process.env.SLACK_API_TOKEN,
+	name: process.env.SLACK_BOT_NAME,
 });
 let channel = process.env.SLACK_CHANNEL;
-let params = {icon_emoji: ':tfws:'};
+let params = {
+	icon_emoji: ':tfws:',
+};
 
 // bot.postMessageToGroup(channel, 'Living Room Has Started', params);
 
 function getData() {
-    Sensor.getJSON().then(Influx.writeInflux).then(function() {
-        setTimeout(getData, Delay);
-    }).catch(function(e) {
-        console.log(e.message);
-	bot.postMessageToGroup(channel, e.message);
-        // Retry
-        setTimeout(getData, Delay);
-    });
+	Sensor.getJSON().then(Influx.writeInflux).then(function() {
+		setTimeout(getData, Delay);
+	}).catch(function(e) {
+		console.log(e.message);
+		bot.postMessageToGroup(channel, e.message);
+		// Retry
+		setTimeout(getData, Delay);
+	});
 };
 
 getData();
